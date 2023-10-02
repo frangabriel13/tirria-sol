83a6598bf3d05f7a4829a84ca68c45e6b4283c06
franco <mansilla.franco.1@gmail.com>
2023-10-02 13:43:35 -0300

    front config

---
+++ client/src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import s from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <div className={s.container}>
+      <h1>Home</h1>
+    </div>
+  );
+};
+
+
+export default Home;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router basename='/'>
+      <App />
+    </Router>
+  </Provider>,
 )
