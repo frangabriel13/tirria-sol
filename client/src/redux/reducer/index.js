1b646edb51f2cee798e7f2da670bb49d28e0dfdb
Franco <mansilla.franco.1@gmail.com>
2023-11-03 19:03:45 -0300

    añadi reducer

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -3,12 +3,14 @@ import productReducer from "./productReducer";
 import imageReducer from "./imageReducer";
 import categoryReducer from "./categoryReducer";
 import sizeReducer from "./sizeReducer";
+import colorReducer from "./colorReducer";
 
 const rootReducer = combineReducers({
   product: productReducer,
   image: imageReducer,
   category: categoryReducer,
   size: sizeReducer,
+  color: colorReducer,
 });
 
 
