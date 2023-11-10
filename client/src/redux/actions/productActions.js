b3c7ccafccebdad081b4a9cd9bdc6f5ef465f35f
Franco <mansilla.franco.1@gmail.com>
2023-11-10 18:37:27 -0300

    reducer de prod

---
+++ client/src/redux/actions/productActions.js
@@ -0,0 +1,61 @@
+import { instance } from '../../utils/axiosConfig';
+
+export const getProducts = () => async (dispatch) => {
+  try {
+    const response = await instance.get('/products');
+    dispatch({
+      type: 'GET_PRODUCTS',
+      payload: response.data
+    })
+  } catch(error) {
+    console.log(error);
+  }
+};
+
+export const getProductById = (id) => async (dispatch) => {
+  try {
+    const response = await instance.get(`/products/${id}`);
+    dispatch({
+      type: 'GET_PRODUCT_BY_ID',
+      payload: response.data
+    })
+  } catch(error) {
+    console.log(error);
+  }
+};
+
+export const addProduct = (product) => async (dispatch) => {
+  try {
+    const response = instance.post('/products', product);
+    dispatch({
+      type: 'ADD_PRODUCT',
+      payload: response.data
+    })
+  } catch(error) {
+    console.log(error);
+  }
+};
+
+export const updateProduct = (payload) => async (dispatch) => {
+  try {
+    const response = instance.put(`/products/${payload.id}`, payload);
+    dispatch({
+      type: 'UPDATE_PRODUCT',
+      payload: response.data
+    })
+  } catch(error) {
+    console.log(error);
+  }
+};
+
+export const deleteProduct = (id) => async (dispatch) => {
+  try {
+    const response = instance.delete(`/products/${id}`);
+    dispatch({
+      type: 'DELETE_PRODUCT',
+      payload: response.data
+    })
+  } catch(error) {
+    console.log(error);
+  }
+};
